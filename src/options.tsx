62ecd37725c814e52e23b1eada725c085b89932d
Trevor Strieber <trevor@strieber.org>
2017-04-21 22:40:41 -0700

    Use webpack w/ react.

---
+++ src/options.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('React', React);
+console.log('ReactDOM', ReactDOM);
+
+ReactDOM.render(
+  <h1>Hello world!</h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const extractCommons = new webpack.optimize.CommonsChunkPlugin({
+  name: 'commons',
+  filename: 'commons.js',
+});
+
+const copyWebpackPlugin = new CopyWebpackPlugin([
+  {
+    from: path.resolve(__dirname, 'src', 'options.html'),
+    to: path.resolve(__dirname, 'dist'),
+  },
+  {
+    from: path.resolve(__dirname, 'src', 'manifest.json'),
+    to: path.resolve(__dirname, 'dist'),
+  },
+]);
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    background: './background.ts',
+    options: './options.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  },
+  plugins: [
+    extractCommons,
+    copyWebpackPlugin,
+  ],
+};
+
+module.exports = config;
