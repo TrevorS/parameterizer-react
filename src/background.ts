c1bcd36a844e27c994010c95c2127c41561e33be
Trevor Strieber <trevor@strieber.org>
2017-04-23 17:58:48 -0700

    Implement basic request modification.

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,21 @@
-console.log('hi');
+import { Pattern } from "./models/pattern";
+import { WebRequest } from "./models/web_request";
+
+const patterns = [
+  new Pattern("Test Pattern", "google.com", [ { key: "test", value: "test2" } ]),
+];
+
+const requestHandler = (webRequest: chrome.webRequest.ResourceRequest) => {
+  const request = new WebRequest(webRequest);
+
+  patterns.forEach((pattern) => {
+    if (request.matches(pattern)) {
+      const newURL = request.addQueryParameters(pattern);
+
+      chrome.tabs.update(request.tabId, { url: newURL.href });
+    }
+  });
+};
+
+chrome.webRequest.onBeforeRequest.addListener(requestHandler,
+  { urls: ["<all_urls>"] }, ["blocking"]);
