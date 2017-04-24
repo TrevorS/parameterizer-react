53e833967d37efe345d8fa4e7cba9a40385755b5
Trevor Strieber <trevor@strieber.org>
2017-04-23 17:58:23 -0700

    More clean up.

--- src/options.tsx
+++ src/options.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-console.log('React', React);
-console.log('ReactDOM', ReactDOM);
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <h1>Hello world!</h1>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
